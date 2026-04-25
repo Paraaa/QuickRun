32bb69738625704e39fda7b4b135c5bf0850de82
Andrej Schwanke <andrejschwanke19@gmail.com>
2026-04-25 17:57:14 +0200

    feat: add color and editing options for groups

--- src/GroupItem.ts
+++ src/GroupItem.ts
@@ -5,7 +5,10 @@ import { QuickRunGroup } from './types';
 export class GroupItem extends vscode.TreeItem {
   constructor(public readonly data: QuickRunGroup) {
     super(data.label, vscode.TreeItemCollapsibleState.Expanded);
-    this.iconPath = new vscode.ThemeIcon(data.icon ?? 'folder');
+    this.iconPath = new vscode.ThemeIcon(
+      data.icon ?? 'folder',
+      data.color ? new vscode.ThemeColor(data.color) : undefined,
+    );
     this.contextValue = 'group';
     this.description = data.source === 'global' ? 'global' : 'project';
     this.id = data.id;
